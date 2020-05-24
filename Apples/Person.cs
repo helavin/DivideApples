0d58db76ef0e28acf1ff75141ff80c882b15025e
helavin <34762647+helavin@users.noreply.github.com>
2020-05-24 21:21:13 +0300

    ТЗ
    Поделить яблоки между детьми

---
+++ Apples/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace Apples
+{
+    class Person
+    {
+        internal bool isNation = false;
+        internal string name;
+        internal int countOfApples = 0;
+
+        internal List<int> apples = new List<int>();
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public Person(bool isNation)
+        {
+            this.isNation = isNation;
+            this.name = "Государства";
+        }
+
+        public void SetCountOfApples()
+        {
+            if (isNation)
+                return;
+            decimal number;
+            Console.Write($"Введите количество яблок для {name}: ");
+            string input = Console.ReadLine();
+
+            bool result = decimal.TryParse(input, out number);
+            if (result == true && number >= 0 && number < int.MaxValue)
+            {
+                countOfApples = (int)number;
+                apples.Add((int)number);
+                Console.WriteLine($"У {name} {countOfApples} яблок.");
+            }
+            else
+            {
+                Console.WriteLine("Упс! Что-то пошло не так...");
+                Console.WriteLine($"У {name} {countOfApples} яблок.");
+            }
+        }        
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"\tУ {name} {countOfApples} яблок.");
+            Console.WriteLine($"\tИстория изменения количества яблок у {name}: {string.Join(", ", apples)}");
+            Console.WriteLine($"\tВсего у {name} {apples.Sum()} яблок!\n");
+        }
+    }
+}
---
+++ Apples/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Жили-были Маша и Витя. Однажды послала их мама за яблоками.");
+
+            List<Person> persons = new List<Person>();
+            Person Masha = new Person("Маши");
+            Person Vitya = new Person("Вити");
+            Person Nation = new Person(true);
+            persons.AddRange(new List<Person> { Masha, Vitya, Nation });
+            bool isFirst = true;
+
+            string ifContinue = "";
+            do
+            {
+                if (!isFirst)
+                {
+                    Console.Write("\n Отправить детей за яблоками ещё раз? (Да - y / Нет - n): ");
+                    ifContinue = Console.ReadLine();
+                }
+                isFirst = false;
+
+                if (!ifContinue.Equals("n"))
+                {
+                    persons.ForEach(p => p.SetCountOfApples());
+                    DivideEqually(ref persons, ref Nation.countOfApples);
+                }
+                else
+                {
+                    Console.WriteLine("Досвидания.");
+                }
+
+            } while (ifContinue != "n");
+        }
+
+        private static void DivideEqually(ref List<Person> persons, ref int countNation)
+        {
+            int amount = 0;
+            persons.Where(x => !x.isNation).ToList()
+                .ForEach(p => amount += p.countOfApples);
+            Console.WriteLine($"Всего у детей: {amount} яблок.");
+
+            Console.Write("Процент яблок для государства: ");
+            decimal percent;
+            var input = Console.ReadLine();
+            bool result = decimal.TryParse(input, out percent);
+            if (result == true && percent > 0 || percent <= 100)
+                countNation = amount * (int)percent / 100;
+            var balance = amount - countNation;
+
+            if (balance % 2 != 0)
+            {
+                balance--;
+                countNation++;
+            }
+
+            Console.WriteLine($"Государству отдали {countNation} яблок, осталось {balance} " +
+                $"\nТеперь поделим яблоки поровну между детьми...\n");
+
+            int equally = balance / 2;
+            foreach (var p in persons)
+            {
+                if (!p.isNation)
+                {
+                    p.countOfApples = equally;
+                    p.apples.Add(-(p.apples.Last() - equally));
+                }
+                else
+                {
+                    p.apples.Add(p.countOfApples);
+                }
+                p.GetInfo();
+            }
+        }
+    }
+}
